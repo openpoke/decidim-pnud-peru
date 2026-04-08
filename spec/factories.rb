8bc56e4e290245aefe343c18076ade04daad7745
David Igón <david@pokecode.net>
2026-04-08 16:44:51 +0200

    improvements in styles of content_blocks

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
 
 require "decidim/core/test/factories"
-require "decidim/verifications/test/factories"
-require "decidim/proposals/test/factories"
 require "decidim/decidim_awesome/test/factories"
