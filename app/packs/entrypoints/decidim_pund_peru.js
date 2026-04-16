17df1a13976e65b18a74de73d478d8570e0a62e9
Ivan Vergés <ivan@pokecode.net>
2026-04-16 17:43:19 +0200

    fix content blocks

--- app/packs/entrypoints/decidim_pund_peru.js
+++
@@ -1,3 +0,0 @@
-import "stylesheets/decidim/pnud_peru/_body.scss"
-require.context("../fonts", true)
-require.context("../images", true)
--- config/initializers/custom_content_blocks.rb
+++ config/initializers/custom_content_blocks.rb
@@ -3,19 +3,31 @@
 Decidim.content_blocks.register(:homepage, :enroll) do |content_block|
   content_block.cell = "decidim/content_blocks/enroll"
   content_block.public_name_key = "decidim.content_blocks.enroll.name"
+  content_block.settings_form_cell = "decidim/content_blocks/enroll_settings_form"
   content_block.default!
+  content_block.settings do |settings|
+    settings.attribute :button_url, type: :string
+  end
 end
 
 Decidim.content_blocks.register(:homepage, :call_to_join) do |content_block|
   content_block.cell = "decidim/content_blocks/call_to_join"
   content_block.public_name_key = "decidim.content_blocks.call_to_join.name"
+  content_block.settings_form_cell = "decidim/content_blocks/call_to_join_settings_form"
   content_block.default!
+  content_block.settings do |settings|
+    settings.attribute :button_url, type: :string
+  end
 end
 
 Decidim.content_blocks.register(:homepage, :welcome) do |content_block|
   content_block.cell = "decidim/content_blocks/welcome"
   content_block.public_name_key = "decidim.content_blocks.welcome.name"
+  content_block.settings_form_cell = "decidim/content_blocks/welcome_settings_form"
   content_block.default!
+  content_block.settings do |settings|
+    settings.attribute :button_url, type: :string
+  end
 end
 
 Decidim.content_blocks.register(:participatory_process_group_homepage, :processes_list) do |content_block|
