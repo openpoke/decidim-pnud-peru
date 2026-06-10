2813c8bc149a4342201c39846512d58c9b17fb23
Ivan Vergés <ivan@pokecode.net>
2026-06-10 11:27:34 +0200

    update decidim. fix fonts

--- config/initializers/content_security_policy.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-# For tuning the Content Security Policy, check the Decidim documentation site
-# https://docs.decidim.org/en/develop/customize/content_security_policy
